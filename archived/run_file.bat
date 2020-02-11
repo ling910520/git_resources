b85a9892aaa8018c243cc734c36aac59b4a11ba6
unknown <you@example.com>
2020-02-11 23:16:25 +0800

    Gitignore issue fixed

--- archived/run_file.bat
+++
@@ -1,2 +0,0 @@
-cd C:\Users\ling910520\Desktop\py\resources
-C:\Users\ling910520\Anaconda3\envs\myclone\python.exe  enabled.py
--- testfilecreated/testFilecreated.py
+++
@@ -1,8 +0,0 @@
-import unittest
-from filecreated import Helper
-import os
-path = r"C:\Users\ling910520\Desktop\c-sharp"
-class TestCheckSize(unittest.TestCase):
-    def testgetsize(self):
-        folder = os.path.getsize(path)
-        self.assertEqual(Helper.checkSize(path),True)
